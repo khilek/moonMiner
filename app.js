8998d894b66572f82ef8ce7395bd75a28adbd7d6
Khile <khilek@gmail.com>
2024-03-23 12:59:36 -0600

    Added in Rover Functionality

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let clickUpgrades = [
     name: 'drill',
     price: 200,
     quantity: 0,
-    multiplier: 5
+    multiplier: 10
   }
 ];
 
@@ -39,11 +39,35 @@ function updateCheeseElm() {
   moonMineElm.innerText = cheese.toString()
 }
 
+function updatePickaxeElm() {
+  const pickaxeUpgrade = clickUpgrades.find(pickaxe => pickaxe.name == 'pickaxe')
+  console.log('pickaxe', pickaxeUpgrade)
+  let pickaxeElm = document.getElementById('pickaxe')
+  pickaxeElm.innerText = `${pickaxeUpgrade.quantity}`
+}
+
+function updateDrillElm() {
+  const drillUpgrade = clickUpgrades.find(drill => drill.name == 'drill')
+  console.log('drill', drillUpgrade)
+  let pickaxeElm = document.getElementById('drill')
+  pickaxeElm.innerText = `${drillUpgrade.quantity}`
+}
+
+
+function updateRoverElm() {
+  const roverUpgrade = automaticUpgrades.find(rover => rover.name == 'rover')
+  console.log('rover', roverUpgrade)
+  let roverElm = document.getElementById('rover')
+  roverElm.innerText = `${roverUpgrade.quantity}`
+}
+
+
+
 
 function buyPickaxe(pickaxeName) {
-  console.log('Buying ⛏️', pickaxeName)
+  // console.log('Buying ⛏️', pickaxeName)
   const pickaxeUpgrade = clickUpgrades.find(pickaxe => pickaxe.name == pickaxeName)
-  console.log('⛏️', pickaxeUpgrade)
+  // console.log('⛏️', pickaxeUpgrade)
   if (cheese >= 5) {
     cheese -= 5
     pickaxeUpgrade.quantity++
@@ -54,26 +78,41 @@ function buyPickaxe(pickaxeName) {
   // console.log('buyPickaxe')
 }
 
-function updatePickaxeElm() {
-  const pickaxeUpgrade = clickUpgrades.find(pickaxe => pickaxe.name == 'pickaxe')
-  console.log('pickaxe', pickaxeUpgrade)
-  let pickaxeElm = document.getElementById('pickaxe')
-  pickaxeElm.innerText = `${pickaxeUpgrade.quantity}`
+
+function buyRover(roverName) {
+  // console.log('Buying ⛏️', pickaxeName)
+  const roverUpgrade = automaticUpgrades.find(rover => rover.name == roverName)
+  // console.log('⛏️', pickaxeUpgrade)
+  if (cheese >= 10) {
+    cheese -= 10
+    roverUpgrade.quantity++
+    updateRoverElm()
+    updatePickaxeElm()
+    updateDrillElm()
+    updateCheeseElm()
+  }
+  // console.log('buyPickaxe')
 }
 
 
-function updateDrillElm() {
-  const drillUpgrade = clickUpgrades.find(drill => drill.name == 'drill')
-  console.log('drill', drillUpgrade)
-  let pickaxeElm = document.getElementById('drill')
-  pickaxeElm.innerText = `${drillUpgrade.quantity}`
-}
+
 
 
 function collectClickUpgrades() {
   clickUpgrades.forEach(clickUpgrade => {
     console.log(clickUpgrade.multiplier)
     cheese += clickUpgrade.multiplier * clickUpgrade.quantity
+    console.log(cheese)
+    updateCheeseElm()
+  })
+
+}
+
+
+function collectAutoUpgrades() {
+  automaticUpgrades.forEach(autoUpgrade => {
+    console.log(autoUpgrade.multiplier)
+    cheese += autoUpgrade.multiplier * autoUpgrade.quantity
     console.log(cheese)
     updateCheeseElm()
   })
